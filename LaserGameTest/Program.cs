b5a87531979de944aaee0cff125e19113e88dfdd
KaiSiegele <109605342+KaiSiegele@users.noreply.github.com>
2024-01-10 07:47:05 +0100

    Add files via upload

---
+++ LaserGameTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using LaserGame;
+using System;
+using System.Collections.Generic;
+
+namespace LaserGameTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Board board = new Board(new List<int>() { 1, 5, 8, 2 }, new List<int> { 4, 2, 3, 2 });
+            var game = new Game();
+            game.Play(board);
+        }
+    }
+}
